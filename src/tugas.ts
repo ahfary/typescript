309c654ea7d2863b498b66413046215cd4d967be
ahfary <afabqory@gmail.com>
2024-08-07 09:57:06 +0700

    update

--- src/tugas.ts
+++ src/tugas.ts
@@ -123,94 +123,88 @@ function Power(a: number, b: number) {
 console.log(Power(7, 3));
 
 // Soal Nomor 3
-// function penjumlahanBilangan(a: number[], b: string): number {
-//     if (b !== "ganjil" && b !== "genap") {
-//         throw new Error("Argument b harus berupa string 'ganjil' atau 'genap'.");
-//     }
+function penjumlahanBilangan(a: number[], b: string): number {
+    if (b !== "ganjil" && b !== "genap") {
+        throw new Error("Argument b harus berupa string 'ganjil' atau 'genap'.");
+    }
 
-//     return a.filter(e => {
-//         if (b === "ganjil") {
-//             return e % 2 === 1;
-//         } else if (b === "genap") {
-//             return e % 2 === 0;
-//         }
-//     }).reduce((acc, curr) => acc + curr, 0);
-// }
-
-function penjumlahanBilangan(a: number[], b: string = "ganjil" || "genap") {
-    return a.filter((e) => {
-        e%2 === 0 && b === "genap" || e%2 === 1 && b === "ganjil"
-    }).reduce((acc, curr) => acc + curr, 0);
+    return a.filter(e => {
+        if (b === "ganjil") {
+            return e % 2 === 1;
+        } else if (b === "genap") {
+            return e % 2 === 0;
+        }
+    }).reduce((acc, curr) => acc + curr);
 }
 
 console.log(penjumlahanBilangan([1,2,3,4,5,6], "ganjil"))
 
 // Soal Number 4
 
-// class Animal {
-//   name: string;
-//   age: number;
-//   isMamali: boolean;
-//   constructor(name: string, age: number, isMamali: boolean) {
-//     this.name = name;
-//     this.age = age;
-//     this.isMamali = isMamali;
-//   }
-// }
+class Animal {
+  name: string;
+  age: number;
+  isMamali: boolean;
+  constructor(name: string, age: number, isMamali: boolean) {
+    this.name = name;
+    this.age = age;
+    this.isMamali = isMamali;
+  }
+}
 
-// // Soal Nomor 5
+// Soal Nomor 5
 
-// class Rabbit extends Animal {
-//   constructor(name: string, age: number, isMamali: boolean) {
-//     super(name, age, isMamali);
-//   }
-//   eat() {
-//     console.log(`${this.name} yang berumur ${this.age} sedang makan!`);
-//   }
-// }
+class Rabbit extends Animal {
+  constructor(name: string, age: number, isMamali: boolean) {
+    super(name, age, isMamali);
+  }
+  eat() {
+    console.log(`${this.name} yang berumur ${this.age} sedang makan!`);
+  }
+}
 
-// // Soal Nomor 6
+// Soal Nomor 6
 
-// class Eagle extends Animal {
-//   speed: number;
-//   constructor(name: string, age: number, isMamali: boolean, speed: number) {
-//     super(name, age, isMamali);
-//     this.speed = speed;
-//   }
+class Eagle extends Animal {
+  speed: number;
+  constructor(name: string, age: number, isMamali: boolean, speed: number) {
+    super(name, age, isMamali);
+    this.speed = speed;
+  }
 
-//   fly() {
-//     console.log(`${this.name} yang berumur ${this.age} sedang terbang!`);
-//   }
+  fly() {
+    console.log(`${this.name} yang berumur ${this.age} sedang terbang!`);
+  }
 
-//   run() {
-//     this.isMamali? console.log(`${this.name} tidak bisa terbang karna nilai is mamalianya true`) : console.log(`${this.name} Berlari dengan kecapatan ${this.speed} km`)
-//   }
-// }
+  run() {
+    this.isMamali? console.log(`${this.name} tidak bisa terbang karna nilai is mamalianya true`) : console.log(`${this.name} Berlari dengan kecapatan ${this.speed} km`)
+  }
+}
 
-// // Soal Nomor 7
+// Soal Nomor 7
 
-// class EagleRun extends Eagle {
-//     constructor(name: string, age: number, isMamali: boolean, speed: number) {
-//         super(name, age, isMamali, speed)
-//     }
-//     // Method run sama flynya udah ada di class Eagle
-// }
+class EagleRun extends Eagle {
+    constructor(name: string, age: number, isMamali: boolean, speed: number) {
+        super(name, age, isMamali, speed)
+    }
+    // Method run sama flynya udah ada di class Eagle
+}
 
-// // Soal Nomor 8
+// Soal Nomor 8
 
-// const myRabbit = new Rabbit("labi", 2, true);
+const myRabbit = new Rabbit("labi", 2, true);
 
-// console.log(myRabbit);
-// myRabbit.eat();
+console.log(myRabbit);
+myRabbit.eat();
 
 
-// // Soal Nomor 9
+// Soal Nomor 9
 
-// const myEagle = new Eagle("Elo", 4, true, 220)
-// myEagle.fly()
-// myEagle.run()
+const myEagle = new Eagle("Elo", 4, true, 220)
+myEagle.fly()
+myEagle.run()
 
-// // Soal Nomor 10
+// Soal Nomor 10
 
-// const speedTes = new EagleRun("Kuku", 12, false, 220)
-// speedTes.run()
+const speedTes = new EagleRun("Kuku", 12, false, 220)
+speedTes.run()
